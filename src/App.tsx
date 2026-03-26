a6b1e8d8a0f3a6d6cceacff7085ccae5b47eafe9
marcosocana <tu@email.com>
2026-03-26 11:24:09 +0100

    Prevent camera scroll reset on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -136,13 +136,11 @@ const App = () => {
     window.addEventListener("resize", updateAppHeight);
     window.addEventListener("orientationchange", updateAppHeight);
     window.visualViewport?.addEventListener("resize", updateAppHeight);
-    window.visualViewport?.addEventListener("scroll", updateAppHeight);
 
     return () => {
       window.removeEventListener("resize", updateAppHeight);
       window.removeEventListener("orientationchange", updateAppHeight);
       window.visualViewport?.removeEventListener("resize", updateAppHeight);
-      window.visualViewport?.removeEventListener("scroll", updateAppHeight);
     };
   }, []);
 
